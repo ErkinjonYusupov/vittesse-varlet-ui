a81e6f1f26d004e084b572ff701e180a9811c25f
Yusupov Erkinjon <erkinnigoradiyora@gmail.com>
2025-10-21 17:22:32 +0500

    ddd

--- src/components/dashboard/categories.vue
+++ src/components/dashboard/categories.vue
@@ -12,14 +12,14 @@ function showProducts(item: any) {
 <template>
  <div mx-auto my-0 max-w-500px p-2>
    <div v-for="(item, index) in categories" :key="index" >
-    <q-card p-4 mb-2 dark:bg-gray-7 rounded-2xl @click="showProducts(item)">
+    <div bg-white dark:bg-gray-7 mb-2 p-4 mx-2 rounded-2xl @click="showProducts(item)">
       <div flex items-center gap-2>
         <Icon :icon="item.icon" width="40" height="40" text-gray-6 dark:text-gray-4/>
         <div text-20px text-gray-6 dark:text-gray-4>
           {{ t(item.title) }}
         </div>
       </div>
-    </q-card>
+    </div>
   </div>
  </div>
 </template>
