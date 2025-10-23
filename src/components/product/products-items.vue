e5a8da0e8e11815b3e34fef802bbe0ede77a00f9
Yusupov Erkinjon <erkinnigoradiyora@gmail.com>
2025-10-23 14:38:51 +0500

    d

--- src/components/product/products-items.vue
+++ src/components/product/products-items.vue
@@ -12,19 +12,26 @@ function addCart(item: any) {
 </script>
 
 <template>
-  <div v-for="item in store.data" :key="item.name" mx-2 mb-2 rounded-2xl bg-white p-4 dark:bg-gray-7>
-    <div flex items-center justify-between>
-      <div>
-        <div text-18px font-700>
-          {{ item.name }}
-        </div>
-        <div mt-2 inline-block border border-green-5 rounded-10px px-3 text-16px text-green-6 font-600>
-          $ {{ item.price }}
+  <div>
+    <div v-if="store.data.length">
+      <div v-for="item in store.data" :key="item.name" mx-2 mb-2 rounded-2xl bg-white p-4 dark:bg-gray-7>
+        <div flex items-center justify-between>
+          <div>
+            <div text-18px font-700>
+              {{ item.name }}
+            </div>
+            <div mt-2 inline-block border border-green-5 rounded-10px px-3 text-16px text-green-6 font-600>
+              $ {{ item.price }}
+            </div>
+          </div>
+          <q-btn flat @click="addCart(item)">
+            <Icon icon="basil:add-outline" width="36px" />
+          </q-btn>
         </div>
       </div>
-      <q-btn flat @click="addCart(item)">
-        <Icon icon="basil:add-outline" width="36px" />
-      </q-btn>
+    </div>
+    <div v-else py-5 text-center text-gray>
+      {{ t('tr33') }}
     </div>
   </div>
 </template>
